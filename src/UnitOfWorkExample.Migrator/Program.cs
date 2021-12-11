5cf1b0ee2e8f52b288710ee4ba7cbe9e378b0b99
Pavel Kirilin <kirilin.pav@gmail.com>
2021-12-11 16:05:16 +0300

    Added non-EF migrator example

---
+++ src/UnitOfWorkExample.Migrator/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Reflection;
+using FluentMigrator.Runner;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace UnitOfWorkExample.Migrator
+{
+    internal static class Program
+    {
+        private static readonly string Env = Environment.GetEnvironmentVariable("ENVIRONMENT");
+        
+        private static int Main()
+        {
+            try
+            {
+                var configuration = CreateConfiguration();
+                var serviceProvider = CreateServiceProvider(configuration);
+                UpdateDatabase(serviceProvider);
+                return 0;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return -1;
+            }
+        }
+
+        private static IServiceProvider CreateServiceProvider(IConfiguration configuration)
+        {
+            var connectionString = configuration["ConnectionStrings:MySql"];
+
+            return new ServiceCollection()
+                .AddFluentMigratorCore()
+                .ConfigureRunner(builder => builder.AddMySql5()
+                    .WithGlobalConnectionString(connectionString)
+                    .ScanIn(Assembly.GetExecutingAssembly()).For.Migrations())
+                .AddLogging(builder => builder.AddFluentMigratorConsole())
+                .BuildServiceProvider(false);
+        }
+
+        private static IConfiguration CreateConfiguration()
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(AppContext.BaseDirectory)
+                .AddJsonFile("appsettings.json");
+
+            if (Env == "Development")
+                builder.AddJsonFile("appsettings.Development.json", optional: true);
+
+            return builder.Build();
+        }
+
+        private static void UpdateDatabase(IServiceProvider serviceProvider)
+        {
+            var runner = serviceProvider.GetRequiredService<IMigrationRunner>();
+            runner.MigrateUp();
+        }
+    }
+}
